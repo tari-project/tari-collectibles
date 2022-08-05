60f39186e9d1b055d7271cc75ac6bb802fe59a41
stringhandler <mikethetike@tari.com>
2022-08-05 14:56:42 +0200

    feat: copy from tari main repo

---
+++ web-app/src/setupTests.js
@@ -0,0 +1,8 @@
+// Copyright 2022 The Tari Project
+// SPDX-License-Identifier: BSD-3-Clause
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
